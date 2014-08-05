de3a8183fdf9fa56c34d134ddcb33e65878c832e
giglio <giglio@chaordicsystems.com>
2014-08-04 23:24:54 -0300

    better plots

--- ThompsonSampling.py
+++ ThompsonSampling.py
@@ -6,9 +6,10 @@ ThompsonSampling.py
 """
 
 
+import operator
 from random import random
 from scipy import stats
-from matplotlib.pyplot import plot, legend, show
+from matplotlib.pyplot import plot, legend, xlabel, show
 
 
 def simulate_bandit(number_of_requests=1000):
@@ -19,65 +20,82 @@ def simulate_bandit(number_of_requests=1000):
     """
 
     # First let's build the alternatives (Bandit's arms)
+
     # Note that we have to define the expected conversion
     # rate of each alternative, quantity that is unknown
-    # (obviously) in actual applications
+    # in actual test applications
     alternatives = {
         'A': {
             'success': 1,
-            'failure': 2,
-            'simulated_conversion': 0.05,
-            'requests_so_far': 0.
+            'failure': 1,
+            'simulated_conversion': 0.1,
+            'requests_so_far': 0.,
+            'tmp_random_beta': 0
         },
         'B': {
             'success': 1,
-            'failure': 2,
-            'simulated_conversion': 0.10,
-            'requests_so_far': 0.
+            'failure': 1,
+            'simulated_conversion': 0.1,
+            'requests_so_far': 0.,
+            'tmp_random_beta': 0
         },
         'C': {
             'success': 1,
-            'failure': 2,
-            'simulated_conversion': 0.15,
-            'requests_so_far': 0.
+            'failure': 1,
+            'simulated_conversion': 0.1,
+            'requests_so_far': 0.,
+            'tmp_random_beta': 0
         }
     }
 
-    # ppAfter the simulation we'll plot the proportions of requests
+    # After the simulation, we'll plot the proportions of requests
     # received by each alternative during time
     proportions_in_time = []
 
     # For each simulated alternative
-    for request_number in range(1, number_of_requests):
+    for _ in range(1, number_of_requests):
 
         # Let's choose one according to the Thompson Sampling procedure
-        chosen_alternative, chosen_alternative_temporary_value = 'A', 0
         for alternative in alternatives:
+            # Get history so far
             success = alternatives[alternative]['success']
             failure = alternatives[alternative]['failure']
-            temporary_values_from_beta = stats.beta.rvs(success, failure)
-            if temporary_values_from_beta > chosen_alternative_temporary_value:
-                chosen_alternative = alternative
-                chosen_alternative_temporary_value = temporary_values_from_beta
+            # Store alternative's own number from Beta
+            alternatives[alternative]['tmp_random_beta'] = stats.beta.rvs(success, failure)
+
+        # Sort by random number from Beta
+        chosen_alternative = sorted(alternatives.iteritems(), 
+                                key=lambda (x, y): y['tmp_random_beta'],
+                                reverse=True)[0][0]
 
         # Feedback is necessary, so we update success and failure
         alternatives[chosen_alternative]['requests_so_far'] += 1
+
+        # Store for plotting
+        list_of_alternatives_requests = [alternatives[alternative]['requests_so_far']
+                                            for alternative in alternatives]
+        requests_so_far = sum(list_of_alternatives_requests)
+        if requests_so_far == 1000:
+            # Store
+            proportions_to_plot = [x/requests_so_far for x in list_of_alternatives_requests]
+            proportions_in_time.append(proportions_to_plot)
+            # Clear
+            for alternative in alternatives:
+                alternatives[alternative]['requests_so_far'] = .0
+
+        # Fake (simulated) conversion
         if random() < alternatives[chosen_alternative]['simulated_conversion']:
             alternatives[chosen_alternative]['success'] += 1
         else:
             alternatives[chosen_alternative]['failure'] += 1
 
-        # Store for plotting
-        proportions_in_time.append([alternatives[alternative]['requests_so_far']/request_number
-                                    for alternative in alternatives])
-
-    plot(proportions_in_time); legend(alternatives.keys()); show()
+    plot(proportions_in_time); legend(alternatives.keys()); xlabel('thousands'); show()
 
 
 def main():
     """ ThompsonSampling.py """
 
-    simulate_bandit(number_of_requests=10000)
+    simulate_bandit(number_of_requests=100000)
 
 
 if __name__ == '__main__':
